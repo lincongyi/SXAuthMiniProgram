56098f3280291b8846e7408a6f56a7a993cc436e
lincongyi <88583354@163.com>
2022-06-07 11:20:31 +0800

    update

--- src/app.config.js
+++ src/app.config.js
@@ -3,19 +3,23 @@ export default defineAppConfig({
     'pages/index/index', // 首页
     'pages/mine/index', // 我的
   ],
-  // tabBar: {
-  //   'custom': true,
-  //   'list': [
-  //     {
-  //       'pagePath': 'pages/index/index',
-  //       'text': '首页'
-  //     },
-  //     {
-  //       'pagePath': 'pages/mine/index',
-  //       'text': '我的'
-  //     }
-  //   ]
-  // },
+  tabBar: {
+    'custom': true,
+    'list': [
+      {
+        selectedIconPath: 'images/tabbar-home-on.png',
+        iconPath: 'images/tabbar-home-off.png',
+        pagePath: 'pages/index/index',
+        text: '首页'
+      },
+      {
+        selectedIconPath: 'images/tabbar-mine-on.png',
+        iconPath: 'images/tabbar-mine-off.png',
+        pagePath: 'pages/mine/index',
+        text: '我的'
+      }
+    ]
+  },
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
